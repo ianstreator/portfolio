7623f41a42e0edc5dd1ab921efda9828cf97df63
Ian <ianstreator+github@gmail.com>
2023-09-07 20:27:27 -0500

    fixed next.config, updated resume

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
-  nextConfig: {
-    reactStrictMode: true,
-  },
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;

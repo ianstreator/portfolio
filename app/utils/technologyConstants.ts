bb59b4515660fa7febe751aac2e310cda442c462
ian <ianstreator+github@gmail.com>
2024-06-19 00:10:52 -0500

    built mapping for technologies and their logo color

---
+++ app/utils/technologyConstants.ts
@@ -0,0 +1,21 @@
+export const technologies: {
+    [normalizedName: string]: { displayName: string; hexColor: string };
+  } = {
+    javascript: { displayName: "JavaScript", hexColor: "#F7DF1E" },
+    typescript: { displayName: "TypeScript", hexColor: "#66C2FF" },
+    tailwind: { displayName: "Tailwind", hexColor: "#5EEAD4" },
+    mongodb: { displayName: "MongoDB", hexColor: "#9CCE97" },
+    daisyui: { displayName: "DaisyUI", hexColor: "#2BD3A8" },
+    react: { displayName: "React.js", hexColor: "#67E8F9" },
+    next: { displayName: "Next.js", hexColor: "#D4D4D8" },
+    node: { displayName: "Node.js", hexColor: "#BEF264" },
+    redis: { displayName: "Redis", hexColor: "#E48481" },
+    jest: { displayName: "Jest", hexColor: "#D194A6" },
+    java: { displayName: "Java", hexColor: "#FFC066" },
+    ruby: { displayName: "Ruby", hexColor: "#CC342D" },
+    scss: { displayName: "Sass", hexColor: "#D98CB2" },
+    css: { displayName: "CSS", hexColor: "#1572B6" },
+    git: { displayName: "Git", hexColor: "#F7886E" },
+    go: { displayName: "Go", hexColor: "#7DDCE8" },
+  };
+  

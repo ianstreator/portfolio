43309b246c2a65fed5b9cee840f71ff233384ced
ian <ianstreator+github@gmail.com>
2025-01-07 23:59:29 -0600

    Refactored and automated the previously hardcoded icon map, reducing repetition and improving file structure.

--- app/utils/reactIcons.tsx
+++ app/utils/reactIcons.tsx
@@ -1,109 +1,81 @@
+import { BsFiletypePdf as ResumeIcon } from "react-icons/bs";
 import {
-  FaReact as ReactIcon,
-  FaSass as SassIcon,
-  FaNodeJs as NodeIcon,
-  FaJava as JavaIcon,
-  FaLinkedinIn as LinkedInIcon,
-  FaFigma as FigmaIcon,
-  FaGitAlt as GitIcon
-} from "react-icons/fa";
+  AiFillGithub as GitHubIcon,
+  AiOutlineMail as EmailIcon,
+} from "react-icons/ai";
 import {
-  TbBrandNextjs as NextIcon,
   TbBrandTypescript as TypeScriptIcon,
+  TbBrandNextjs as NextIcon,
 } from "react-icons/tb";
-import { TbBrandVscode as VScodeIcon } from "react-icons/tb";
 import {
   BiLogoTailwindCss as TailwindIcon,
+  BiLogoVisualStudio as VScodeIcon,
   BiLogoMongodb as MongodbIcon,
 } from "react-icons/bi";
 import {
-  SiDaisyui as DaisyuiIcon,
-  SiJest as JestIcon,
-  SiRedis as RedisIcon,
   SiContentful as ContentfulIcon,
   SiPostgresql as PostgresqlIcon,
+  SiDaisyui as DaisyuiIcon,
   SiPostman as PostmanIcon,
-  SiDocker as DockerIcon
+  SiDocker as DockerIcon,
+  SiRedis as RedisIcon,
+  SiJest as JestIcon,
 } from "react-icons/si";
-
-import { technologies } from "./technologyConstants";
-
-import { AiFillGithub as GitHubIcon } from "react-icons/ai";
-import { MdOutlineEmail as EmailIcon } from "react-icons/md";
-import { BsFiletypePdf as ResumeIcon } from "react-icons/bs";
-
+import {
+  FaLinkedinIn as LinkedInIcon,
+  FaReact as ReactIcon,
+  FaFigma as FigmaIcon,
+  FaNodeJs as NodeIcon,
+  FaGitAlt as GitIcon,
+  FaSass as SassIcon,
+  FaJava as JavaIcon,
+} from "react-icons/fa";
+import { IconType } from "react-icons";
+const importMap: { [key: string]: IconType } = {
+  ContentfulIcon,
+  PostgresqlIcon,
+  TypeScriptIcon,
+  LinkedInIcon,
+  TailwindIcon,
+  DaisyuiIcon,
+  PostmanIcon,
+  MongodbIcon,
+  VScodeIcon,
+  ResumeIcon,
+  GitHubIcon,
+  DockerIcon,
+  ReactIcon,
+  FigmaIcon,
+  RedisIcon,
+  EmailIcon,
+  NodeIcon,
+  SassIcon,
+  JavaIcon,
+  NextIcon,
+  JestIcon,
+  GitIcon,
+};
+import { assetProps } from "./technologyConstants";
+export const ICON_SIZE = 30;
 type IconMap = {
   [name: string]: React.JSX.Element;
 };
-export const ICON_SIZE = 30;
-
-export const contactIconMap: IconMap = {
-  email: <EmailIcon size={ICON_SIZE} className="m-3" />,
-  github: <GitHubIcon size={ICON_SIZE} className="m-3" />,
-  resume: <ResumeIcon size={ICON_SIZE} className="m-3" />,
-  linkedin: <LinkedInIcon size={ICON_SIZE} className="m-3" />,
-};
-export const techIconMap: IconMap = {
-  [technologies.next.displayName]: (
-    <NextIcon
-      size={ICON_SIZE}
-      color={technologies.next.hexColor}
-      strokeWidth={1}
-    />
-  ),
-  [technologies.react.displayName]: (
-    <ReactIcon size={ICON_SIZE} color={technologies.react.hexColor} />
-  ),
-  [technologies.tailwind.displayName]: (
-    <TailwindIcon size={ICON_SIZE} color={technologies.tailwind.hexColor} />
-  ),
-  [technologies.node.displayName]: (
-    <NodeIcon size={ICON_SIZE} color={technologies.node.hexColor} />
-  ),
-  [technologies.scss.displayName]: (
-    <SassIcon size={ICON_SIZE} color={technologies.scss.hexColor} />
-  ),
-  [technologies.typescript.displayName]: (
-    <TypeScriptIcon
-      size={ICON_SIZE}
-      color={technologies.typescript.hexColor}
-      strokeWidth={1}
-    />
-  ),
-  [technologies.java.displayName]: (
-    <JavaIcon size={ICON_SIZE} color={technologies.java.hexColor} />
-  ),
-  [technologies.mongodb.displayName]: (
-    <MongodbIcon size={ICON_SIZE} color={technologies.mongodb.hexColor} />
-  ),
-  [technologies.daisyui.displayName]: (
-    <DaisyuiIcon size={ICON_SIZE} color={technologies.daisyui.hexColor} />
-  ),
-  [technologies.jest.displayName]: (
-    <JestIcon size={ICON_SIZE} color={technologies.jest.hexColor} />
-  ),
-  [technologies.redis.displayName]: (
-    <RedisIcon size={ICON_SIZE} color={technologies.redis.hexColor} />
-  ),
-  [technologies.contentful.displayName]: (
-    <ContentfulIcon size={ICON_SIZE} color={technologies.contentful.hexColor} />
-  ),
-  [technologies.figma.displayName]: (
-    <FigmaIcon size={ICON_SIZE} color={technologies.figma.hexColor} strokeWidth={.5} />
-  ),
-    [technologies.postgresql.displayName]: (
-    <PostgresqlIcon size={ICON_SIZE} color={technologies.postgresql.hexColor} />
-  ),
-  [technologies.vscode.displayName]: (
-    <VScodeIcon size={ICON_SIZE} color={technologies.vscode.hexColor} strokeWidth={1} />
-  ),
-  [technologies.postman.displayName]: (
-    <PostmanIcon size={ICON_SIZE} color={technologies.postman.hexColor} strokeWidth={.1} />
-  ),
-  [technologies.docker.displayName]: (
-    <DockerIcon size={ICON_SIZE} color={technologies.docker.hexColor} strokeWidth={.1} />
-  ),
-  [technologies.git.displayName]: (
-    <GitIcon size={ICON_SIZE} color={technologies.git.hexColor} strokeWidth={.1} />
-  ),
-};
+export const iconMap: IconMap = Object.keys(importMap).reduce((acc, icon) => {
+  const normalizedName = icon.toLowerCase().replace("icon", "");
+  const {
+    displayName,
+    hexColor,
+    strokeWidth = 0.01,
+  } = assetProps[normalizedName];
+  const IconElement = importMap[icon];
+  return {
+    ...acc,
+    [displayName]: (
+      <IconElement
+        size={ICON_SIZE}
+        color={hexColor}
+        strokeWidth={strokeWidth}
+      />
+    ),
+  };
+}, {});
